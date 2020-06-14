dcb61ae1739a36403c9f747fe1d5b6ec44a539c5
Denis <thedenisnikulin@gmail.com>
2020-06-14 23:31:27 +0300

    Updates

---
+++ precache-manifest.c3734a705c8e905eac6e96dd201ab54b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930294d41ecca09dce9d4fc33f00c800",
+    "url": "/chattitude-app/index.html"
+  },
+  {
+    "revision": "48715eb7024d0db1b8fe",
+    "url": "/chattitude-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b0a78ba43eac29796f3d",
+    "url": "/chattitude-app/static/js/2.9d03a15d.chunk.js"
+  },
+  {
+    "revision": "f0b25a5e5afe7b057a262e459f8d43f1",
+    "url": "/chattitude-app/static/js/2.9d03a15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48715eb7024d0db1b8fe",
+    "url": "/chattitude-app/static/js/main.26bf2778.chunk.js"
+  },
+  {
+    "revision": "fcdd7ee06bd94cb8d7c5",
+    "url": "/chattitude-app/static/js/runtime-main.a51f539c.js"
+  },
+  {
+    "revision": "3b2aa637e895c4a4dfc70025c4eb1c65",
+    "url": "/chattitude-app/static/media/auth.3b2aa637.svg"
+  },
+  {
+    "revision": "7e9057c51df00c47c2ab168ffea0ca91",
+    "url": "/chattitude-app/static/media/av0.7e9057c5.svg"
+  },
+  {
+    "revision": "14efb9e99e26805e8aed696d9eddd9c2",
+    "url": "/chattitude-app/static/media/av1.14efb9e9.svg"
+  },
+  {
+    "revision": "e5dd2777024af4451d9ef94aec6782de",
+    "url": "/chattitude-app/static/media/av2.e5dd2777.svg"
+  },
+  {
+    "revision": "e4c82f5722a8fb1aef59b1823f90214c",
+    "url": "/chattitude-app/static/media/av3.e4c82f57.svg"
+  },
+  {
+    "revision": "3ca31b86a56087843b44a38736a7738c",
+    "url": "/chattitude-app/static/media/av4.3ca31b86.svg"
+  },
+  {
+    "revision": "913434a9d2f958038aee33f0dbf22e72",
+    "url": "/chattitude-app/static/media/av5.913434a9.svg"
+  },
+  {
+    "revision": "e32e043177da6a0da30c29f8dc8fca56",
+    "url": "/chattitude-app/static/media/text-logo.e32e0431.svg"
+  }
+]);
